ec367bfe0bb20dd8de9144c4903e9f0959b99b43
mariaconejo <mary03081@gmail.com>
2020-11-15 23:02:02 -0600

    cambio 3 jala los datos  y los pone dentro de un acordion

--- js/script.js
+++ js/script.js
@@ -4,34 +4,57 @@
 
 function addMovies(movies) {
   const movieList = document.getElementById('movie-list');
+  movieList.innerHTML = '';
+  window.scroll(0, 0);
 
   for (let i = 0; i < movies.length; i += 1) {
     const movieItem = document.createElement('li');
     movieList.appendChild(movieItem);
+    fetch(`http://www.omdbapi.com/?apikey=c6fbafc9&i=${movies[i].imdbID}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const content = `
+            <div class="wrapper clearfix">
+              <button class="acordeon">${movies[i].Title}</button>
+              <div class="info">
+                <img class="Imagen_1" src="${movies[i].Poster}" alt="${movies[i].Title}">
+                <p>${movies[i].Title}</p>
+                <p>${movies[i].Year}</p>
+                <p>${data.Runtime}</p>
+                <p>${data.Metascore}</p>
+                <p>${data.Plot}</p>
+                <p>${data.Actors}</p>
+              </div>
+            </div>
+            `;
+        movieItem.innerHTML = content;
 
-    const content = `
-      <div class="left">
-        <img src="${movies[i].Poster}" alt="Foto de ${movies[i].Title}"/>
-      </div>
-      <div class="right">
-        <h2 class="character-name">${movies[i].Title}</h2>
-        <p>${movies[i].Year}</p>
-        <p>${movies[i].imdbID}</p>
-      </div>
-    `;
-    movieItem.innerHTML = content;
+        const acordeon = document.querySelectorAll('.acordeon');
+        console.log(acordeon);
+        for (let i = 0; i < acordeon.length; i += 1) {
+          acordeon[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            const elemento = event.currentTarget;
+            const info = elemento.nextElementSibling;
+            if (info.style.display === 'block') {
+              info.style.display = 'none';
+            } else {
+              info.style.display = 'block';
+            }
+          });
+        }
+      });
   }
 }
 
-fetch('http://www.omdbapi.com/?apikey=c6fbafc9&s=anime')
-  .then((response) => response.json())
-  .then((data) => {
-    addMovies(data.Search);
-  });
-
-fetch('http://www.omdbapi.com/?apikey=c6fbafc9&i=tt0829442')
-  .then((response) => response.json())
-  .then((data) => {
-    addMovies(data.imdbID);
-    console.log(data);
-  });
+const search = document.getElementById('searchform');
+search.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchValue = search.elements[0].value;
+  console.log(search);
+  fetch(`http://www.omdbapi.com/?apikey=c6fbafc9&s=${searchValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      addMovies(data.Search);
+    });
+});
